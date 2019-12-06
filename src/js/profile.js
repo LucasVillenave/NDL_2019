51278b5654828c3e8913ed90b14fdff66cd59583
lucas villenave <lucas.villenave@etu.u-bordeaux.fr>
2019-12-06 03:58:01 +0100

    profile

--- src/js/profile.js
+++ src/js/profile.js
@@ -3,10 +3,13 @@ class MainPage {
         this.caractéristiques = {
             Nom: "Pelaise",
             Prénom: "Jon",
+            Age: 19,
             Logement: "Non Définit",
-            Revenus: 0,
+            Revenus: 18000,
             Montant_compte: 0,
-            ratachement_fiscal: "parents"
+            ratachement_fiscal: "parents",
+            Travail: "aucun",
+            Aides: "aucunes"
         };
         this.init();
     }
@@ -18,12 +21,18 @@ class MainPage {
         $('#resp_profile4').text(this.caractéristiques.Revenus);
         $('#resp_profile5').text(this.caractéristiques.Montant_compte);
         $('#resp_profile6').text(this.caractéristiques.ratachement_fiscal);
+        $('#resp_profile12').text(this.caractéristiques.Age);
+        $('#resp_profile42').text(this.caractéristiques.Travail);
+        $('#resp_profilesss').text(this.caractéristiques.Aides);
         $('#resp_profile1').addClass("profile_class");
         $('#resp_profile2').addClass("profile_class");
         $('#resp_profile3').addClass("profile_class");
         $('#resp_profile4').addClass("profile_class");
         $('#resp_profile5').addClass("profile_class");
         $('#resp_profile6').addClass("profile_class");
+        $('#resp_profile12').addClass("profile_class");
+        $('#resp_profile42').addClass("profile_class");
+        $('#resp_profilesss').addClass("profile_class");
     }
 }
 
