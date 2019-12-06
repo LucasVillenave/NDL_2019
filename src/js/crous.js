b52327842987277c047c9b8d46161bbe3560c535
Tanguy_BERASATEGUY <tanguy.berasateguy@etu.u-bordeaux.fr>
2019-12-06 02:35:05 +0100

    crous: manque retour -> modifs page main et autoriser APL sinon CHECK

--- src/js/crous.js
+++ src/js/crous.js
@@ -3,42 +3,31 @@
 class MainPage {
     constructor ()
     {
-        this.option = {
-            favorite: false,
-            Recent: true
-        };
+        this.nom="Pelaise";
+        this.prenom="Jon";
         this.init();
-    }
-
-    toggle_content(page)
-    {
-        switch (page) {
-            case "step1":
-                break;
-            case "bye":
-                $("#elemClic").hide();
-                $("#affichage").fadeIn("fast");
-                break;
-        }
     }
 
     init()
     {
         let tmp;
-        
-        this.toggle_content("step1");
 
         //Click on add Img
-        tmp = $("#valider");
+        tmp = $("#validerCrous");
+        $("#nomCrous").val("");
+        $("#prenomCrous").val("");
         tmp.on('click', () => {
-            console.log($("#nomCrous").val());
-            console.log($("#prenomCrous").val());
-        });
-        tmp = $("#affichage");
-        tmp.on('click', () => {
-            this.toggle_content("coucou");
-        });
-        
+            if($("#nomCrous").val()==this.nom && $("#prenomCrous").val()==this.prenom){
+                $("#resultatCrous").text("Nom correct !");
+                $("#resultatCrous").addClass("green");
+                $("#resultatCrous").removeClass("red");
+            }
+            else{
+                $("#resultatCrous").text("Nom et/ou Prenom incorrect !");
+                $("#resultatCrous").addClass("red");
+                $("#resultatCrous").removeClass("green");
+            }
+        });        
     }
 }
 
